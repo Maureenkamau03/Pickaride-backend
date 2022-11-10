b25e96c116c6348a1122a147f695a8ec103eec16
geogrey12muiruri <greggambugua@gmail.com>
2022-11-11 02:56:23 +0300

    COL-20 <this commit seeds a user data into the database>

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,5 @@ puts "creating cars"
 car1 = Car.create(car_description: "Chevrolet Optra", mileage: 1000, images: "https://i.postimg.cc/HnwbjVvk/optra.webp", registration_no: "KCB 112Z", is_hired: false)
 car2 = Car.create(car_description: "Toyota Prius", mileage: 1000, images: "https://i.postimg.cc/GhJhgksp/prius.webp", registration_no: "KDB 102J", is_hired: false)
 
+user1 = User.create(name: "greg", email: "Switch@gmail.com", location: "mitini", licence: "G0060", password: "gggu9bje")
 
