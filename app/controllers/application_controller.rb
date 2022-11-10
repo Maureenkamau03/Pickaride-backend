ef1295d8527ee37e1e8cc6ea2b229afdff12b34f
Maureenkamau03 <mkaymaureen@gmail.com>
2022-11-10 17:01:27 +0300

    COL-23 config.ru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  get '/cars' do
-    car = Car.all
-    car.to_json
-  end
-  get '/cars/:car_description' do
-    car = Car.find_by(params[:car_description])
-    car.to_json
-  end
+  # get '/cars' do
+  #   car = Car.all
+  #   car.to_json
+  # end
+  # get '/cars/:car_description' do
+  #   car = Car.find_by(params[:car_description])
+  #   car.to_json
+  # end
 end
