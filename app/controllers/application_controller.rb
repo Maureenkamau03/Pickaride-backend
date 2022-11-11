1299b90f51a216c4a315b2eee94a85a1641ce88f
Maureenkamau03 <mkaymaureen@gmail.com>
2022-11-11 13:08:54 +0300

    COL-23 post request to rentals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,20 @@ class ApplicationController < Sinatra::Base
     car = Car.find_by(params[:car_description])
     car.to_json
   end
+  get 'cars/:id' do
+    car = Car.find(params[:id])
+  end
+  post 'rentals/' do
+    rentals = Rental.create(car_description: params[:first], 
+      registration_no: params[:registration_no],
+      pickup_date: params[:'pick_up'])
+    end
 end
+
+# employees = Employee.create(first: params[:first],  
+#   last: params[:last], 
+#   email: params[:email], 
+#   work_station: params[:work_station],
+#   phone: params[:phone],
+#   hobby: params[:hobby])
+# employees.to_json
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,53 @@
 puts "creating cars"
-car1 = Car.create(car_description: "Chevrolet Optra", mileage: 1000, images: "https://i.postimg.cc/HnwbjVvk/optra.webp", registration_no: "KCB 112Z", is_hired: false)
-car2 = Car.create(car_description: "Toyota Prius", mileage: 1000, images: "https://i.postimg.cc/GhJhgksp/prius.webp", registration_no: "KDB 102J", is_hired: false)
+# car1 = Car.create(car_description: "Chevrolet Optra", mileage: 1000, images: "https://i.postimg.cc/HnwbjVvk/optra.webp", registration_no: "KCB 112Z", is_hired: false)
+# car2 = Car.create(car_description: "Toyota Prius", mileage: 1000, images: "https://i.postimg.cc/GhJhgksp/prius.webp", registration_no: "KDB 102J", is_hired: false)
 
-
+Car.create ([
+  {car_description: "Chevrolet Optra",
+   mileage: 1000,
+   images: "https://i.postimg.cc/HnwbjVvk/optra.webp",
+   registration_no: "KCB 112Z",
+   is_hired: false},
+   {
+    car_description: "Toyota Prius", 
+    mileage: 1000,
+    images: "https://i.postimg.cc/GhJhgksp/prius.webp",
+    registration_no: "KDB 102J",
+    is_hired: false
+   },
+   {
+    car_description: "Honda insight", 
+    mileage: 10000,
+    images: "https://i.postimg.cc/vBbg9rPn/2010-Honda-Insight-LX-10-03-2009.jpg",
+    registration_no: "KDF 189J",
+    is_hired: false
+   },
+   {
+    car_description: "Mazda Demio", 
+    mileage: 5000,
+    images: "https://i.postimg.cc/zBbMChVJ/MAZDA.jpg",
+    registration_no: "KBB 200J",
+    is_hired: false
+   }
+   {
+    car_description: "Honda swift", 
+    mileage: 15000,
+    images: "https://i.postimg.cc/vBbg9rPn/2010-Honda-Insight-LX-10-03-2009.jpg",
+    registration_no: "KDF 189J",
+    is_hired: false
+   },
+   {
+    car_description: "Honda insight", 
+    mileage: 10000,
+    images: "https://i.postimg.cc/vBbg9rPn/2010-Honda-Insight-LX-10-03-2009.jpg",
+    registration_no: "KDF 189J",
+    is_hired: false
+   },
+   {
+    car_description: "Honda insight", 
+    mileage: 10000,
+    images: "https://i.postimg.cc/vBbg9rPn/2010-Honda-Insight-LX-10-03-2009.jpg",
+    registration_no: "KDF 189J",
+    is_hired: false
+   },
+])
