892d18568b73069db3598049eb07a52a1427e9c5
geogrey12muiruri <greggambugua@gmail.com>
2022-11-13 14:42:54 +0300

    COL-20 <added the find_by_id route>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 class UsersController < ApplicationController
-    get '/users' do
-        # get all the users from the database
-        users = User.all.order(:created_at)
-        # users = User.where.not(first: [nil, "Untitled"])
-        # User.where(first: [values]).delete_all
-        # User.where.not(first: 'Untiltled').where.not(first: 'nil')
-        # users.where.not(first: nil)
-        # send them back as a JSON array
-        users.to_json
-      end
+  get '/users/:id' do
+    user = User.find_by_id(params[:id])
+    user.to_json
+  end
+  
 
     post '/users' do
         users = User.create(first: params[:first],  
@@ -19,15 +14,17 @@ class UsersController < ApplicationController
           password: params[:password])
         users.to_json
       end
-     
+
+
+     #sets up the route for performing a get request for the username
       get "/users/:username" do
         user = User.find_by_username(params[:username])
-        user.to_json(include: [:trips])
+        user.to_json
       end
       get "/users/:username" do
         user = User.find_by_username(params[:username])
           if user
-            user.to_json(include: [:trips])
+            user.to_json
           else
             status 401
             { errors: "user doesn't exist" }.to_json
